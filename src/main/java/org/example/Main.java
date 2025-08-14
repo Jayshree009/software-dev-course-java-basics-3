6f34c122ce9e1247a06d8e54b290519749543750
Jaya <jaya.kolte21@gmail.com>
2025-08-14 14:32:42 -0500

    code changes done.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,29 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        JavaCafe javaCafe = new JavaCafe();
-        javaCafe.run();
+        ConditionalExercises ce = new ConditionalExercises();
+        LoopExercises le = new LoopExercises();
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter any number : ");
+        int num = input.nextInt();
+        System.out.println(ce.lessThanFive(num));
+
+        System.out.println("Enter your age : ");
+        int age = input.nextInt();
+        System.out.println(ce.getAgeGroup(age));
+
+        System.out.println("Enetr your password : ");
+        String password = input.next();
+        System.out.println( ce.isValidPassword(password));
+
+
+        System.out.println("Enter any digit :");
+        int num1 = input.nextInt();
+        System.out.println("sum(number): " + le.sum(num1));
+        System.out.println("sumUntilEven(num): " + le.sumUntilEven(num1));
     }
 }
